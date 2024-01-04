12e04a2eed9801f25640bfacf6b2d6a2ffc5f703
letsummer <letsummerparty@gmail.com>
2024-01-04 15:12:26 +0900

    메인 페이지에서 보일 전체 지도 파일 추가

--- client/src/component/Map.js
+++ client/src/component/Map.js
@@ -1,78 +1,71 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 const { kakao } = window;
 
-function Map({searchPlace}){
+function Map(){
+
+    const [positions, setPosition] = useState([]);
+
+    const getData = async () => {
+        const json = await (
+          await fetch(
+            `http://localhost:5000/api/list`
+          )
+        ).json();
+        
+        json.map((item, index)=>{
+            const result = {
+                name: item.name,
+                address: item.address,
+                phone: item.phone,
+                coord: new kakao.maps.LatLng(item.coord[1], item.coord[0]),
+                info: item.info
+            };
+            setPosition((position)=>[...position, result]);
+        });
+    }
+
+    useEffect(()=>{
+        getData();
+    },[]);
+    
+    console.log(`useState로 생성한 positions: `, positions);
+
     useEffect(() => {
-        const infowindow = new kakao.maps.InfoWindow({ zIndex: 1 })
-        const container = document.getElementById('map')
+        // const infowindow = new kakao.maps.InfoWindow({ zIndex: 1 });
+        const container = document.getElementById("map");
         const options = {
-            center: new kakao.maps.LatLng(33.450701, 126.570667),
-            level: 3,
+            center: new kakao.maps.LatLng(37.41675241461202, 126.88494020113373),
+            level: 5,
         }
-        const map = new kakao.maps.Map(container, options)
-        const ps = new kakao.maps.services.Places()
-        ps.keywordSearch(searchPlace, placesSearchCB)
-    
-        function placesSearchCB(data, status, pagination) {
-            if (status === kakao.maps.services.Status.OK) {
-                let bounds = new kakao.maps.LatLngBounds()
+        const map = new kakao.maps.Map(container, options);
+
+        // 마커 이미지의 이미지 주소입니다
+        var imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png"; 
         
-                for (let i = 0; i < data.length; i++) {
-                    displayMarker(data[i])
-                    bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x))
-                }
-                map.setBounds(bounds)
-            }
-        }
-    
-        function displayMarker(place) {
-            let marker = new kakao.maps.Marker({
-                map: map,
-                position: new kakao.maps.LatLng(place.y, place.x),
-            });
-    
-            // 마커에 클릭이벤트를 등록합니다
-            kakao.maps.event.addListener(marker, 'mouseover', ()=>{
-                // 마커위에 마우스를 올리면면 장소명이 인포윈도우에 표출됩니다
-                infowindow.setContent('<div style="padding:5px;font-size:12px;">' + place.place_name + '</div>');
-                infowindow.open(map, marker);
-                
-            });
-
-            kakao.maps.event.addListener(marker, 'click', ()=>{
-                console.log(`장소 정보: `, place);
-                // let resultDiv = document.getElementById('result'); 
-                // resultDiv.innerHTML = place.place_name;
-                
-                let formContent = document.getElementById("addform");
-                formContent.name.value = place.place_name;
-                formContent.addr.value = place.road_address_name;
-                formContent.phone.value = place.phone;
-                // formContent.innerHTML = place.place_name;
-
-            });
+        for (var i = 0; i < positions.length; i ++) {
+            // 마커 이미지의 이미지 크기 입니다
+            var imageSize = new kakao.maps.Size(24, 35); 
             
-        }
-        
-    }, [searchPlace]);
+            // 마커 이미지를 생성합니다    
+            var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize); 
+            
+            // console.log(`for문에서의 [${i}]번째 positions: `, positions);
+            // 마커를 생성합니다
+            var marker = new kakao.maps.Marker({
+                map: map, // 마커를 표시할 지도
+                position: positions[i].coord, // 마커를 표시할 위치
+                title : positions[i].name, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+                image : markerImage // 마커 이미지 
+            });
+
+        }        
+    });
 
     return(
-        <div>
-            <hr />
-            <div
-                id="map"
-                style={{
-                    width: '500px',
-                    height: '200px',
-                }}>
-            </div>
-            <form id="addform" action="">
-                <input id="name" type="text" placeholder="가게명" disabled/>
-                <input id="addr" type="text" placeholder="도로명 주소" disabled/>
-                <input id="phone" type="text" placeholder="연락처" disabled/>
-            </form>
-            
-            {/* <input type="text" value={searchPlace}/> */}
+        <div id="map" style={{
+            width: "500px",
+            height:"500px"
+        }}>
         </div>
     );
 }
