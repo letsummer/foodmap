1023836f1340a0dcac5f05cd39e3e6c2603f725d
letsummer <letsummerparty@gmail.com>
2024-01-05 14:38:00 +0900

    list를 post, get 모두 설정

--- server/routers/rootRouter.js
+++ server/routers/rootRouter.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { getList, getConfirm } from "../controllers/rootCtrl.js";
+import { getList, postList, getConfirm } from "../controllers/rootCtrl.js";
 import { getForm, postForm } from "../controllers/placeCtrl.js";
 
 const rootRouter = express.Router();
 
-rootRouter.get(`/list`, getList);
+rootRouter.route(`/list`).get(getList).post(postList);
 rootRouter.get(`/confirm`, getConfirm);
 rootRouter.route('/add').get(getForm).post(postForm);
 
