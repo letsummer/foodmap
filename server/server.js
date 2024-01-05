7526db14eb2e52d6c0b3f3396544a534d53464f0
letsummer <letsummerparty@gmail.com>
2024-01-05 10:51:58 +0900

    서버파일 수정

--- server/server.js
+++ server/server.js
@@ -2,7 +2,9 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import "./db.js";
-import { getPlace } from "./controllers/placeCtrl.js";
+// import { getList, postList } from "./controllers/rootCtrl.js";
+import rootRouter from "./routers/rootRouter.js";"./routers/rootRouter.js";
+import placeRouter from "./routers/placeRouter.js";"./routers/placeRouter.js";
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -12,6 +14,9 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.route(`/api/list`).get(getPlace);
+app.use("/api", rootRouter);
+app.use("/api/place", placeRouter);
+
+// app.route(`/api/list`).get(getList).post(postList);
 
 app.listen(port, ()=> console.log(`✅ Listening on port ${port}`));
