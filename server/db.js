e55fdeb16ba3d814eed3363fea36f797c7310e16
letsummer <letsummerparty@gmail.com>
2024-01-08 18:13:06 +0900

    dburi를 직접 넣기

--- server/db.js
+++ server/db.js
@@ -2,11 +2,11 @@
 import "dotenv/config";
 import mongoose from "mongoose";
 
-const uri= process.env.DB_URI;
+// const uri= process.env.DB_URI;
 
 async function connect(){
     try {
-        await mongoose.connect(uri);
+        await mongoose.connect(process.env.DB_URI);
         console.log("✅ Connected to Mongodb");
     } catch (error) {
         console.error(error);
