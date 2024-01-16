80ca716c2efa91b7a00acb8977870f367e36a51d
letsummer <letsummerparty@gmail.com>
2024-01-16 16:02:08 +0900

    서버 파일 수정

---
+++ server/middlewares.js
@@ -0,0 +1,9 @@
+export const localsMiddleware = (req, res, next) =>{
+    // console.log(`###req.session: `, req.session);
+    // console.log(`###req.session.user: `, req.session.user);
+    // // console.log(`###req.session.loggedIn: `, req.session);
+    // console.log(`###req.sessionStore.all: `, req.sessionStore.all);
+    // console.log(`###req.sessionStore.all: `, req.sessionStore.all);
+
+    // next();
+}
