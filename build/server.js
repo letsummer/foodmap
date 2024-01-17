9ef4f94669dc913a765fac9ae2a67193e2408a4d
letsummer <letsummerparty@gmail.com>
2024-01-17 15:29:28 +0900

    cors 설정중 (cors를 default 값으로)

--- build/server.js
+++ build/server.js
@@ -12,14 +12,12 @@ import { localsMiddleware } from "./middlewares.js";
 import "dotenv/config";
 const app = express();
 const port = process.env.PORT || 5000;
-
-// app.use(cors());
-app.use(cors({
-  origin: [`${process.env.CLIENT_URL}`],
-  //(Whatever your frontend url is) 
-  methods: ["GET", "POST"],
-  credentials: true
-}));
+app.use(cors());
+// app.use(cors({
+//     origin : [`${process.env.CLIENT_URL}`],  //(Whatever your frontend url is) 
+//     methods: ["GET", "POST"],
+//     credentials: true,
+// }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
--- server/server.js
+++ server/server.js
@@ -14,12 +14,12 @@ import "dotenv/config";
 const app = express();
 const port = process.env.PORT || 5000;
 
-// app.use(cors());
-app.use(cors({
-    origin : [`${process.env.CLIENT_URL}`],  //(Whatever your frontend url is) 
-    methods: ["GET", "POST"],
-    credentials: true,
-}));
+app.use(cors());
+// app.use(cors({
+//     origin : [`${process.env.CLIENT_URL}`],  //(Whatever your frontend url is) 
+//     methods: ["GET", "POST"],
+//     credentials: true,
+// }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
