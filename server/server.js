b1233d46fdb4e6a99dc6b13187645e9afe542f6a
letsummer <letsummerparty@gmail.com>
2024-01-17 15:58:08 +0900

    cors 설정 중 (app.use 추가)

--- server/server.js
+++ server/server.js
@@ -13,12 +13,33 @@ import "dotenv/config";
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use((req, res, next) => {
+    res.setHeader(
+      "Access-Control-Allow-Origin",
+      "https://foodmap-hdef.onrender.com"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    res.setHeader("Access-Control-Allow-Private-Network", true);
+    //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+    res.setHeader("Access-Control-Max-Age", 7200);
+  
+    next();
+  });
 // app.use(cors());
-app.use(cors({
-    origin : [`${process.env.CLIENT_URL}`],  //(Whatever your frontend url is) 
-    methods: ["GET", "POST"],
-    credentials: true,
-}));
+// app.use(cors({
+//     // origin : [`${process.env.CLIENT_URL}`],  //(Whatever your frontend url is) 
+//     methods: ["GET", "POST"],
+//     credentials: true,
+// }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
