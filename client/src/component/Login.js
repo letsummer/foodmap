c35080f257fead23a65f46cde7db2274dbc98400
letsummer <letsummerparty@gmail.com>
2024-01-17 15:33:34 +0900

    fetch mode options를 cors로

--- client/src/component/Login.js
+++ client/src/component/Login.js
@@ -23,7 +23,7 @@ function Login(){
         e.preventDefault();
 
         await fetch(`${process.env.REACT_APP_SERVER_URL}/api/users/login`,{
-            mode: "no-cors",
+            mode: "cors",
             method: "POST",
             credentials: "include",
             headers: {
